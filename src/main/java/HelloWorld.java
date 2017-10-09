93a3fb5af1f6986339d15b3d0260166f80fdbec4
shiningtone <liu633leilei@163.com>
2017-10-09 16:15:37 +0800

    add

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by administer on 2017/10/9.
+ */
+public class HelloWorld {
+    public static void main()
+    {
+        System.out.println("sb");
+    }
+}
---
+++ src/test/test.java
@@ -0,0 +1,16 @@
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+/**
+ * Created by administer on 2017/10/9.
+ */
+public class test {
+    @Test
+    public void run()
+    {
+        String str=null;
+        System.out.println("sb");
+        Boolean s=StringUtils.isBlank(str);
+        System.out.println(s);
+    }
+}
